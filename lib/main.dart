17f8ac2d453dce1921cbacccf9033bce46556630
RiaSastraDewi <riasastrad@gmail.com>
2024-06-08 16:16:16 +0700

    add check in/out

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:presensi_app/screen/splash.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+Future<void> main() async {
+  //format tanggal indonesia
+  await initializeDateFormatting('id', null);
   runApp(const MainApp());
 }
 
